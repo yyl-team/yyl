3f1c9f7b1b3c5376fb60dd5f5b4aede6cfaa370e
jackNEss <jackness1208@qq.com>
2016-11-13 20:57:49 +0800

    [EDIT] 项目初始化 命令功能完成

--- tasks/w-init.js
+++ tasks/w-init.js
@@ -2,6 +2,7 @@
 var 
     color = require('../lib/colors'),
     util = require('../lib/yyl-util'),
+    vars = util.vars,
     path = require('path'),
     fs = require('fs'),
     inquirer = require('inquirer');
@@ -19,12 +20,8 @@ var
                     name: 'name',
                     message: 'name',
                     type: 'input',
-                    default: path.dirname(process.cwd()).split(/[\/\\]/).pop(),
-                }, {
-                    name: 'version',
-                    message: 'version',
-                    type: 'input',
-                    default: '0.1.0'
+                    default: vars.PROJECT_PATH.split('/').pop(),
+
                 }, {
                     name: 'platforms',
                     message: 'platform',
@@ -126,7 +123,144 @@ var
                     next(data);
                 }
             });
-        }).then(function(data, next){
+        }).then(function(data){
+            var 
+                parentDir = util.joinFormat(vars.PROJECT_PATH).split('/').pop(),
+                frontPath = '',
+                initClientFlow = function(dirname, workflowName, done){
+                    util.msg.info('init client', workflowName, 'start');
+                    var dirPath = path.join(frontPath, dirname);
+                    if(!fs.existsSync(dirPath)){
+                        fs.mkdirSync(dirPath);
+                    }
+                    if(fs.readdirSync(dirPath).length){
+                        done(dirname + ' directory is not empty, init fail')
+                        return;
+                    }
+
+                    util.copyFiles(
+                        path.join(vars.BASE_PATH, 'init-files', workflowName),
+                        dirPath,
+                        function(err){
+                            if(err){
+                                return done('copy file error, init fail');
+                            }
+                            util.msg.success('init client', workflowName, 'success');
+                            done();
+                        },
+                        /package\.json|node_modules|gulpfile.js|.DS_Store/g,
+                        null,
+                        path.join(vars.PROJECT_PATH, frontPath)
+                    );
+                },
+                initServerPackage = function(workflowName, done){
+                    util.msg.info('init server', workflowName, 'start');
+                    var workflowPath = path.join(vars.SERVER_PATH, workflowName);
+                    if(!fs.existsSync(vars.SERVER_PATH)){
+                        fs.mkdirSync(vars.SERVER_PATH);
+                    }
+
+                    if(!fs.existsSync(workflowPath)){
+                        fs.mkdirSync(workflowPath);
+                    }
+                    var files = [],
+                        fileParam = {};
+
+                    switch(workflowName){
+                        case 'gulp-requirejs':
+                        case 'webpack-vue':
+                            files = ['package.json', 'gulpfile.js'];
+                            break;
+
+                        default:
+                            files = ['package.json'];
+                            break;
+                    }
+                    files.forEach(function(filePath){
+                        fileParam[path.join(vars.BASE_PATH, 'init-files', workflowName, filePath)] = path.join(workflowPath, filePath);
+                    });
+
+                    new util.Promise(function(next){ // copy files
+                        util.copyFiles(fileParam, function(err){
+                            if(err){
+                                util.msg.error('copy file to serverpath fail', err);
+                                return;
+                            }
+                            util.msg.info('copy file to serverpath pass');
+                            next();
+                        });
+
+                    }).then(function(next){ // cd to the path
+                        util.runCMD('cd ' + workflowPath, function(err){
+                            if(err){
+                                util.msg.error('change to dir '+ workflowPath +' fail');
+                                return;
+                            }
+                            util.msg.info('cd '+ workflowPath +' pass');
+                            next();
+
+                        });
+
+                    }).then(function(next){ // npm install 
+
+                        util.runCMD('npm install', function(err){
+                            if(err){
+                                util.msg.error('npm install fail on server!');
+                                return;
+                            }
+                            util.msg.info('npm install success');
+                            next();
+
+                        }, workflowPath);
+
+                    }).then(function(next){ // back to dirPath
+                        util.runCMD('cd ' + vars.PROJECT_PATH, function(err){
+                            if(err){
+                                util.msg.error('change to dir '+ vars.PROJECT_PATH +' fail');
+                                return;
+                            }
+                            util.msg.info('cd '+ vars.PROJECT_PATH +' pass');
+                            next();
+
+                        });
+
+                    }).then(function(next){ // back to dirPath
+                        util.msg.success('init server', workflowName, 'success');
+                        if(done){
+                            done();
+                        }
+                    }).start();
+                    
+                };
+
+            if(parentDir !== data.name){ // 如项目名称与父级名称不一致, 创建顶级目录
+                fs.mkdirSync(data.name);
+                frontPath = data.name;
+            }
+
+            var padding = 0,
+                paddingCheck = function(err){
+                    if(err){
+                        util.msg.error(err);
+                    }
+                    padding--;
+
+                    if(!padding){
+                        util.msg.line().success(data.name + ' init complete');
+
+                    }
+
+                };
+            if(data.pcWorkflow){
+                padding += 2;
+                initClientFlow('pc', data.pcWorkflow, paddingCheck);
+                initServerPackage(data.pcWorkflow, paddingCheck);
+            }
+            if(data.mobileWorkflow){
+                padding += 2;
+                initClientFlow('mobile', data.mobileWorkflow, paddingCheck);
+                initServerPackage(data.mobileWorkflow, paddingCheck);
+            }
 
         }).start();
 
