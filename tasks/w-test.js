e6626a5059c0858d7846fa5631be7e2f3fdd5bab
jackNEss <jackness1208@qq.com>
2016-11-18 00:42:36 +0800

    [EDIT] 扩展 buildtree 函数

--- tasks/w-test.js
+++ tasks/w-test.js
@@ -6,8 +6,9 @@ var
     wTest = function(){
         util.buildTree({
             // frontPath: 'path01/path02/path03',
-            path: 'F:/svn/code.yy.com/ent-FEteam/_test/pc',
-            dirNoDeep: ['dist', 'html', 'css', 'js', 'templates', 'components', 'sass']
+            // path: 'F:/svn/code.yy.com/ent-FEteam/_test/pc',
+            path: '/Volumes/sd128G/work/git/yy/code.yy.com/ent-FEteam/yy.com',
+            dirNoDeep: ['dist', 'html', 'css', 'js', 'templates', 'components', 'sass', 'node_modules', '.git', '.sass-cache']
         });
 
     };
