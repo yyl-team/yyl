e21810c7095c1543f4aba01298c851011fd306f0
jackness <jackness1208@qq.com>
2017-04-10 19:50:40 +0800

    [ADD] 新增 rollup workflow 打包方式

---
+++ examples/rollup/multi-project/src/proj01/components/r-demo/r-demo.js
@@ -0,0 +1,20 @@
+'use strict';
+var 
+    wDemo = {
+        init: function(){
+            var logoEl = document.getElementById('logo');
+            var i;
+            var iClass = [0, 1, 2, 3];
+
+            setInterval(function(){
+                var here = iClass.concat([]);
+                here.splice(here.indexOf(i), 1);
+
+                i = here[Math.round(Math.random() * (here.length - 1))];
+                logoEl.className = 'w-demo-logo w-demo-logo-' + i;
+            }, 2000);
+        }
+    };
+
+
+export default wDemo;
