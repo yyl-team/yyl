0472672c78c51a95f05ca6f963cd8c243b73bc8d
jackness <jackness1208@qq.com>
2017-12-07 14:50:14 +0800

    + * [ADD] `gulp-requirejs`, `gulp-rollup` 新增 `--remote` 选项

--- tasks/w-update.js
+++ tasks/w-update.js
@@ -4,9 +4,9 @@ var wServer = require('./w-server.js');
 
 var
     update = function(version){
-        var iCmd = 'git pull origin/master';
+        var iCmd = 'git pull -u origin master';
         if(version){
-            iCmd = 'git pull origin/' + version;
+            iCmd = 'git pull -u origin ' + version;
         }
         util.msg.info(iCmd);
     };
