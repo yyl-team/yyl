d2065f39186a907b714433f430d83318750cd4b6
jackNEss <jackness1208@qq.com>
2018-07-30 16:52:58 +0800

    让 example 新增 mock 例子

--- examples/webpack/single-project/src/components/widget/w-demo/w-demo.js
+++ examples/webpack/single-project/src/components/widget/w-demo/w-demo.js
@@ -1,9 +1,17 @@
+import axios from 'axios';
 
 const wDemo = {
   init() {
     const logoEl = document.getElementById('logo');
+    const tEl = document.getElementById('t');
     let i;
     const iClass = [0, 1, 2, 3];
+
+    axios.get('/api/mock/list/1').then((res) => {
+      tEl.innerHTML = res.data.title;
+    }).catch((er) => {
+      throw new Error(er);
+    });
 
     setInterval(() => {
       const here = iClass.concat([]);
