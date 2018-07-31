48f8356e09e13f3bfcb8d691ea03c02f92ced6cd
jackNEss <jackness1208@qq.com>
2018-07-31 15:11:21 +0800

    更新 webpack-vue2-for-pc 初始化种子

--- examples/webpack-vue2/single-project-for-pc/src/components/page/p-sub/p-sub.js
+++ examples/webpack-vue2/single-project-for-pc/src/components/page/p-sub/p-sub.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import { mapGetters, mapActions } from 'vuex';
 
-import tpl from './p-index.pug';
-import './p-index.scss';
+import tpl from './p-sub.pug';
+import './p-sub.scss';
 import vDemo from '../../widget/v-demo/v-demo.vue';
 import vNav from '../../widget/v-nav/v-nav.vue';
 
@@ -24,6 +24,6 @@ export default Vue.extend({
   },
   mounted() {
     const vm = this;
-    vm.addDemoLog('p-index is ready');
+    vm.addDemoLog('p-sub is ready');
   }
 });
