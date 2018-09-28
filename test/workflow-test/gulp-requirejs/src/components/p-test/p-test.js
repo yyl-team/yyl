dfa86418ba3b1e8a108d96398d48519190da792f
jackNEss <jackness1208@qq.com>
2018-09-28 17:36:36 +0800

    添加用例

--- test/workflow-test/gulp-requirejs/src/components/p-test/p-test.js
+++ test/workflow-test/gulp-requirejs/src/components/p-test/p-test.js
@@ -1,9 +1,11 @@
 'use strict';
-require(['artTemplate'], function(artTemplate) {
+require(['artTemplate', 'yyloader'], function(artTemplate, yyloader) {
     var cntEl = document.getElementById('tmplCnt');
     cntEl.innerHTML = artTemplate('tmpl', {
         url: '//yyweb.yystatic.com/pc/images/components/w-head/images/icons-head.png',
         url2: __url('../../images/logo.png'),
         url3: '../../images/test/logo.png/123'.replace('/123', '')
     });
+
+    yyloader('#tLoaderEl');
 });
---
+++ test/workflow-test/gulp-requirejs/src/components/t-demo/t-demo.js
@@ -0,0 +1,3 @@
+require(['wSubWidget'], function(wSubWidget) {
+    wSubWidget.init();
+});
