1ae0892f55723bbb89cb4efa3d19fee2d6ed1f72
jackNEss <jackness1208@qq.com>
2019-01-31 19:30:35 +0800

    log 改造

--- tasks/w-log.js
+++ tasks/w-log.js
@@ -1,5 +1,6 @@
 'use strict';
-const util = require('./w-util.js');
+const util = require('yyl-util');
+const print = require('yyl-print');
 const chalk = require('chalk');
 
 const cache = {
@@ -17,456 +18,124 @@ const cache = {
   }
 };
 
-util.infoBar.init({
-  head: {
-    key: {
-      'help': {
-        name: 'HELP',
-        color: 'white',
-        bgColor: 'bgBlue'
-      },
-      'info': {
-        name: 'INFO',
-        color: 'white',
-        bgColor: 'bgBlue'
-      },
-      'init': {
-        name: 'INIT',
-        color: 'white',
-        bgColor: 'bgBlue'
-      },
+print.log.init({
+  type: {
+    'help': {
+      name: 'HELP',
+      color: 'white',
+      bgColor: 'bgBlue'
+    },
+    'info': {
+      name: 'INFO',
+      color: 'white',
+      bgColor: 'bgBlue'
+    },
+    'init': {
+      name: 'INIT',
+      color: 'white',
+      bgColor: 'bgBlue'
+    },
 
-      'server': {
-        name: 'SERV',
-        color: 'white',
-        bgColor: 'bgBlue'
-      },
-      'watch': {
-        name: 'WATC',
-        color: 'white',
-        bgColor: 'bgBlue'
-      },
-      'optimize': {
-        name: 'OPTI',
-        color: 'white',
-        bgColor: 'bgRed'
-      },
-      'rebuild': {
-        name: 'REBU',
-        color: 'white',
-        bgColor: 'bgBlue'
-      },
-      'proxy': {
-        name: 'PROX',
-        color: 'white',
-        bgColor: 'bgBlack'
-      },
-      'proxy2': {
-        name: 'PROX',
-        color: 'black',
-        bgColor: 'bgWhite'
-      },
-      'done': {
-        name: 'DONE',
-        color: 'black',
-        bgColor: 'bgGreen'
-      },
-      'warn': {
-        name: 'WARN',
-        color: 'white',
-        bgColor: 'bgYellow'
-      },
-      'success': {
-        name: 'PASS',
-        color: 'white',
-        bgColor: 'bgCyan'
-      },
-      // + w-commit
-      'commit-copy': {
-        name: 'COPY',
-        color: 'white',
-        bgColor: 'bgBlue'
-      },
-      'commit-step01': {
-        name: 'ST01',
-        color: 'white',
-        bgColor: 'bgBlue'
-      },
-      'commit-step02': {
-        name: 'ST02',
-        color: 'white',
-        bgColor: 'bgBlue'
-      },
-      'commit-step03': {
-        name: 'ST03',
-        color: 'white',
-        bgColor: 'bgBlue'
-      },
-      // - w-commit
-      // + make
-      'make': {
-        name: 'MAKE',
-        color: 'white',
-        bgColor: 'bgBlue'
-      },
-      // - make
-      // + w-remove
-      'remove': {
-        name: 'RM',
-        color: 'white',
-        bgColor: 'bgBlue'
-      },
-      // - w-remove
-      // + w-update
-      'update': {
-        name: 'UPDT',
-        color: 'white',
-        bgColor: 'bgBlue'
-      },
-      'cmd': {
-        name: 'CMD>',
-        color: 'white',
-        bgColor: 'bgBlack'
-      },
-      'yyl': {
-        name: 'YYL>',
-        color: 'white',
-        bgColor: 'bgBlack'
-      }
-      // - w-update
+    'server': {
+      name: 'SERV',
+      color: 'white',
+      bgColor: 'bgBlue'
+    },
+    'watch': {
+      name: 'WATC',
+      color: 'white',
+      bgColor: 'bgBlue'
+    },
+    'optimize': {
+      name: 'OPTI',
+      color: 'white',
+      bgColor: 'bgRed'
+    },
+    'rebuild': {
+      name: 'REBU',
+      color: 'white',
+      bgColor: 'bgBlue'
+    },
+    'proxy': {
+      name: 'PROX',
+      color: 'white',
+      bgColor: 'bgBlack'
+    },
+    'proxy2': {
+      name: 'PROX',
+      color: 'black',
+      bgColor: 'bgWhite'
+    },
+    'done': {
+      name: 'DONE',
+      color: 'black',
+      bgColor: 'bgGreen'
+    },
+    'warn': {
+      name: 'WARN',
+      color: 'white',
+      bgColor: 'bgYellow'
+    },
+    'success': {
+      name: 'PASS',
+      color: 'white',
+      bgColor: 'bgCyan'
+    },
+    // + w-commit
+    'commit-copy': {
+      name: 'COPY',
+      color: 'white',
+      bgColor: 'bgBlue'
+    },
+    'commit-step01': {
+      name: 'ST01',
+      color: 'white',
+      bgColor: 'bgBlue'
+    },
+    'commit-step02': {
+      name: 'ST02',
+      color: 'white',
+      bgColor: 'bgBlue'
+    },
+    'commit-step03': {
+      name: 'ST03',
+      color: 'white',
+      bgColor: 'bgBlue'
+    },
+    // - w-commit
+    // + make
+    'make': {
+      name: 'MAKE',
+      color: 'white',
+      bgColor: 'bgBlue'
+    },
+    // - make
+    // + w-remove
+    'remove': {
+      name: 'RM',
+      color: 'white',
+      bgColor: 'bgBlue'
+    },
+    // - w-remove
+    // + w-update
+    'update': {
+      name: 'UPDT',
+      color: 'white',
+      bgColor: 'bgBlue'
+    },
+    'cmd': {
+      name: 'CMD>',
+      color: 'white',
+      bgColor: 'bgBlack'
+    },
+    'yyl': {
+      name: 'YYL>',
+      color: 'white',
+      bgColor: 'bgBlack'
     }
+    // - w-update
   }
 });
-
-const fn = {
-  matchKey(str, word, r) {
-    return str
-      .replace(new RegExp(` ${word}$`, 'g'), ` ${r}`)
-      .replace(new RegExp(`^${word} `, 'g'), `${r} `)
-      .replace(new RegExp(` ${word} `, 'g'), ` ${r} `)
-      .replace(new RegExp(` ${word}([:,.]+)`, 'g'), ` ${r}$1`);
-  },
-  costFormat(cost) {
-    let min;
-    let sec;
-    let us;
-    if (cost < 1000) {
-      return `${cost} ms`;
-    } else if (cost < 1000 * 60) {
-      sec = Math.floor(cost / 1000);
-      us = cost % 1000;
-      return `${sec} s ${us} ms`;
-    } else {
-      min = Math.floor(cost / 1000 / 60);
-      sec = Math.floor(cost / 1000) % 60;
-      us = cost % 1000;
-      return `${min} min ${sec} s ${us} ms`;
-    }
-  }
-};
-
-const log4Detail = (module, type, argv) => {
-  let cost;
-  switch (module) {
-    case 'start':
-      cache.timer[type] = new Date();
-      cache.currentType = type;
-      util.msg.info(`[${type}] task start`);
-      break;
-
-    case 'finished':
-    case 'finish':
-      if (cache.currentType) {
-        cost = new Date() - cache.timer[cache.currentType];
-        argv.push(`[${cache.currentType}] task finished, cost ${fn.costFormat(cost)}`);
-        util.msg.success(...argv);
-      }
-      break;
-
-    case 'cmd':
-      if (!argv.length) {
-        argv = [type];
-      }
-      util.msg.cmd(...argv);
-      break;
-
-    case 'yyl':
-      if (!argv.length) {
-        argv = [type];
-      }
-      util.msg.yyl(...argv);
-      break;
-
-    case 'clear':
-      util.msg.info(chalk.yellow('clear screen'));
-      break;
-
-    case 'msg':
-      if (!util.msg[type]) {
-        type = 'info';
-      }
-
-      switch (type) {
-        case 'optimize':
-        case 'create':
-        case 'del':
-        case 'update':
-          util.msg[type](util.path.relative(util.vars.PROJECT_PATH, argv[0]));
-          break;
-
-        case 'concat':
-          argv = argv.map((src) => {
-            return util.path.relative(util.vars.PROJECT_PATH, src);
-          });
-          util.msg[type](`${chalk.cyan(argv[0])} <= [${chalk.yellow(argv.slice(1).join(','))}]`);
-          break;
-
-        default:
-          util.msg[type](...argv);
-          break;
-      }
-      break;
-  }
-};
-
-const log4Base = (module, type, argv) => {
-  let iStatus;
-  let cost;
-
-  if (module == 'start') {
-    cache.timer[type] = new Date();
-    cache.currentType = type;
-    iStatus = cache.status[cache.currentType] = {
-      errors: [],
-      warns: [],
-      adds: [],
-      success: [],
-      updates: [],
-      dels: [],
-      rev: [],
-      optimizes: [],
-      defaultText: argv.join(' ')
-    };
-  } else {
-    iStatus = cache.status[cache.currentType];
-  }
-
-  if ((module == 'finish' || module == 'finished') && type) {
-    argv = [type].concat(argv);
-  }
-
-  if (!iStatus && module != 'clear' && module != 'cmd' && module != 'yyl') {
-    return log4Detail(module, type, argv);
-  }
-
-  const prinitInfo = () => {
-    if (cache.isEnd) {
-      return;
-    }
-    let leftArr = [];
-    let rightArr = [];
-    if (iStatus.optimizes.length) {
-      leftArr.push(`${chalk.red('OPTIMIZE')} ${chalk.yellow(iStatus.optimizes.length)}`);
-    }
-
-    if (iStatus.adds.length) {
-      leftArr.push(`${chalk.green('ADD')} ${chalk.yellow(iStatus.adds.length)}`);
-    }
-
-    if (iStatus.updates.length) {
-      leftArr.push(`${chalk.cyan('UPDATE')} ${chalk.yellow(iStatus.updates.length)}`);
-    }
-
-    if (iStatus.dels.length) {
-      leftArr.push(`${chalk.gray('DEL')} ${chalk.yellow(iStatus.dels.length)}`);
-    }
-
-    if (iStatus.errors.length) {
-      rightArr.push(chalk.red(`${iStatus.errors.length} errors`));
-    }
-
-    if (iStatus.warns.length) {
-      rightArr.push(chalk.yellow(`${iStatus.warns.length} warning`));
-    }
-
-    util.infoBar.print(cache.currentType, {
-      foot: util.getTime(),
-      barLeft: leftArr.join(' ') || iStatus.defaultText,
-      barRight: rightArr.join(' ')
-    });
-  };
-
-  switch (module) {
-    case 'start':
-      if (!cache.isEnd) {
-        util.infoBar.end();
-      }
-      cache.isEnd = false;
-      clearInterval(cache.timeIntervalKey);
-      cache.timeIntervalKey = setInterval(() => {
-        prinitInfo();
-      }, 1000);
-      prinitInfo();
-      break;
-
-    case 'clear':
-      cache.isEnd = true;
-      util.cleanScreen();
-      break;
-
-    case 'cmd':
-      cache.isEnd = true;
-      if (!argv.length) {
-        argv = [type];
-      }
-      util.infoBar.print('cmd', {
-        barLeft: chalk.cyan(argv.join(' ')),
-        foot: util.getTime()
-      }).end();
-      break;
-
-    case 'yyl':
-      cache.isEnd = true;
-      if (!argv.length) {
-        argv = [type];
-      }
-      util.infoBar.print('yyl', {
-        barLeft: chalk.cyan(argv.join(' ')),
-        foot: util.getTime()
-      }).end();
-      break;
-
-    case 'finished':
-    case 'finish':
-      cache.isEnd = true;
-      util.infoBar.end();
-      clearInterval(cache.timeIntervalKey);
-      cost = new Date() - cache.timer[cache.currentType];
-      if (iStatus.errors.length) {
-        let allStr = true;
-        iStatus.errors.some((argv) => {
-          argv.some((arg) => {
-            if (typeof arg !== 'string') {
-              allStr = false;
-              return true;
-            }
-          });
-          if (!allStr) {
-            return true;
-          }
-        });
-        if (allStr) {
-          util.infoBar.print('error', {
-            barLeft: iStatus.errors.map((a) => a.join(' '))
-          });
-        } else {
-          util.infoBar.print('error', {
-            barRight: `cost ${fn.costFormat(cost)}`,
-            foot: util.getTime()
-          }).end();
-          iStatus.errors.forEach((argv) => {
-            console.error(...argv);
-          });
-        }
-      } else {
-        util.infoBar.print('done', {
-          barLeft: argv.join(' '),
-          barRight: `cost ${fn.costFormat(cost)}`
-        });
-        util.infoBar.print('success', {
-          barLeft: iStatus.success.concat(iStatus.rev).map((a) => a.join(' '))
-        });
-      }
-
-      util.infoBar.print('warn', {
-        barLeft: iStatus.warns.map((a) => a.join(' '))
-      }).end();
-
-      delete cache.status[cache.currentType];
-      cache.currentType = null;
-      break;
-
-    case 'end':
-      cache.isEnd = true;
-      util.infoBar.end();
-      break;
-
-    case 'msg':
-      argv = argv.map((ctx) => {
-        let r = ctx;
-        if (typeof ctx == 'string') {
-          r = fn.matchKey(r, 'finished', chalk.green('finished'));
-          r = fn.matchKey(r, 'failed', chalk.red('failed'));
-          r = fn.matchKey(r, 'error', chalk.red('error'));
-        }
-        return r;
-      });
-      switch (type) {
-        case 'create':
-          cache.isEnd = false;
-          iStatus.adds.push(argv);
-          break;
-
-        case 'update':
-          cache.isEnd = false;
-          iStatus.updates.push(argv);
-          break;
-
-        case 'del':
-          cache.isEnd = false;
-          iStatus.dels.push(argv);
-          break;
-
-        case 'success':
-          iStatus.success.push(argv);
-          break;
-
-        case 'error':
-          cache.isEnd = false;
-          iStatus.errors.push(argv);
-          break;
-
-        case 'warn':
-          cache.isEnd = false;
-          iStatus.warns.push(argv);
-          break;
-
-        case 'rev':
-          iStatus.success.push(['rev:'].concat(argv));
-          break;
-
-        case 'optimize':
-          cache.isEnd = false;
-          iStatus.optimizes.push(argv);
-          break;
-
-        case 'concat':
-          argv = argv.map((src) => {
-            return util.path.relative(util.vars.PROJECT_PATH, src);
-          });
-          iStatus.success.push([`concat: ${chalk.cyan(argv[0])}`]);
-          iStatus.success.push([`<= [${chalk.yellow(argv.slice(1).join(','))}]`]);
-          break;
-
-        default:
-          break;
-      }
-      prinitInfo();
-      break;
-  }
-};
-
-const log4Silent = (module, type, argv) => {
-  switch (module) {
-    case 'msg':
-      switch (type) {
-        case 'error':
-          util.msg[type](...argv);
-          break;
-      }
-      break;
-  }
-};
 
 const log = (module, type, argv) => {
   let iArgv = [];
@@ -476,13 +145,42 @@ const log = (module, type, argv) => {
   if (!~cache.logLevel) {
     cache.logLevel = 1;
   }
-  switch (+cache.logLevel) {
-    case 0:
-      return log4Silent(module, type, iArgv);
-    case 1:
-      return log4Base(module, type, iArgv);
-    case 2:
-      return log4Detail(module, type, iArgv);
+
+  print.log.setLogLevel(+cache.logLevel);
+
+  switch (module) {
+    case 'start':
+      if (type in print.log[type]) {
+        print.log[type](...iArgv);
+      }
+      break;
+    case 'clear':
+      print.cleanScreen();
+      break;
+
+    case 'cmd':
+      print.log.cmd(...iArgv);
+      break;
+
+    case 'yyl':
+      print.log.yyl(...iArgv);
+      break;
+
+    case 'finished':
+    case 'finish':
+    case 'end':
+      break;
+
+    case 'msg':
+      if (type in print.log) {
+        print.log[type](...iArgv);
+      } else {
+        print.log.info(...iArgv);
+      }
+      break;
+
+    default:
+      break;
   }
 };
 log.update = (lv) => {
