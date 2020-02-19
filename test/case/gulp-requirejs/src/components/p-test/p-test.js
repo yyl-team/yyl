a21fed465faf4e57fd378d8e0f4a9cbdf0a85925
liudaojie <liudaojie@yy.com>
2020-02-19 08:46:41 +0800

    feat: 让 yyl 支持 `__html()` 语法糖，用于引入 tmpl 文件

--- test/case/gulp-requirejs/src/components/p-test/p-test.js
+++ test/case/gulp-requirejs/src/components/p-test/p-test.js
@@ -1,5 +1,6 @@
 'use strict';
 require(['artTemplate', 'yyloader'], function(artTemplate, yyloader) {
+    var tpl = __html('{$srcRoot}/components/p-test/test-tmpl.html')
     var cntEl = document.getElementById('tmplCnt');
     cntEl.innerHTML = artTemplate('tmpl', {
         url: '//yyweb.yystatic.com/pc/images/components/w-head/images/icons-head.png',
@@ -7,5 +8,6 @@ require(['artTemplate', 'yyloader'], function(artTemplate, yyloader) {
         url3: '../../images/test/logo.png/123'.replace('/123', '')
     });
 
+    console.log(tpl)
     yyloader('#tLoaderEl');
 });
