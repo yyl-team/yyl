d49ad0374caf125402b9c5f868e6a1b89c5b9ef9
liudaojie <liudaojie@yy.com>
2021-05-18 07:27:26 +0800

    feat: 更新依赖

--- tasks/seed.js
+++ tasks/seed.js
@@ -52,7 +52,7 @@ async function seed({ logger, env, cmds, shortEnv }) {
 seed.packages = [
   {
     name: 'yyl-seed-webpack',
-    version: '3.0.6'
+    version: '3.0.7'
   },
   {
     name: 'yyl-seed-gulp-requirejs',
